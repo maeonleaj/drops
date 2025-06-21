60dc867044f42e445173d3ac0428a12e0820fa8a
maeonleaj <maeonleaj@gmail.com>
2025-06-21 05:34:00 -0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from 'next'
 const isProd = process.env.NODE_ENV === 'production'
 
 const nextConfig: NextConfig = {
-  output: 'export',
+  /* output: 'export', */
   basePath: isProd ? '/drops' : '',
   assetPrefix: isProd ? '/drops/' : '',
   images: {
